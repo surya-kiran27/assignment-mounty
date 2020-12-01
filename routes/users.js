193620c775d1e6919aaf92545dda8467a5e33964
surya-kiran27 <suryaraju67@gmail.com>
2020-12-02 00:16:30 +0530

    added update and delete user endpoints

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const e = require("express");
 var express = require("express");
 var router = express.Router();
 const User = require("../models/user");
-
+//create user
 router.post("/", async (req, res) => {
   //check if mobile number is associated with another user
   const mobile_exists = await User.findOne({ mobile: req.body.mobile });
@@ -38,7 +38,61 @@ router.post("/", async (req, res) => {
     res.json({ success: true, user: response });
   } catch (error) {
     console.log(error);
-    res.json({ success: false, message: "Error occured try again!" });
+    res.json({
+      success: false,
+      message: "Failed to add user,Error occured try again!",
+    });
+  }
+});
+//update user
+router.put("/update", async (req, res) => {
+  //check if user exists using there mobile number
+  const user_exists = await User.findOne({ mobile: req.body.mobile });
+  if (user_exists == null) {
+    res.json({ success: false, message: "User Does not exist" });
+    return;
+  }
+  //build the updated user object
+  const updatedUser = {
+    name: req.body.name,
+    mobile: req.body.mobile,
+    email: req.body.email,
+    address: {
+      street: req.body.street,
+      locality: req.body.locality,
+      city: req.body.city,
+      state: req.body.state,
+      pincode: req.body.pincode,
+      coordinatesType: req.body.coordinatesType,
+      coordinates: req.body.coordinates.split(",").map(Number),
+    },
+  };
+  try {
+    const response = await User.findOneAndUpdate(
+      { mobile: req.body.mobile },
+      updatedUser,
+      { new: true }
+    );
+    res.json({ success: true, user: response });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Failed to update user,Error occured try again!",
+    });
+  }
+});
+//delete the user form the db
+router.delete("/delete", async (req, res) => {
+  try {
+    //find the user in db using mobile number
+    await User.findOneAndDelete({ mobile: req.body.mobile });
+    res.json({ success: true, message: "User Deleted Successfully!" });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "Failed to delete user, Error occured try again!",
+    });
   }
 });
 
